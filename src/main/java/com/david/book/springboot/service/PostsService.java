462541c12430070932201ec40cd5ec430f0754b6
Jangdawit <david023@naver.com>
2020-12-24 04:18:13 +0900

    Update post

--- src/main/java/com/david/book/springboot/service/PostsService.java
+++ src/main/java/com/david/book/springboot/service/PostsService.java
@@ -1,8 +1,11 @@
 package com.david.book.springboot.service;
 
 
+import com.david.book.springboot.domain.posts.Posts;
 import com.david.book.springboot.domain.posts.PostsRepository;
+import com.david.book.springboot.web.dto.PostsResponseDto;
 import com.david.book.springboot.web.dto.PostsSaveRequestDto;
+import com.david.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(
+                "해당 게시글이 없습니다. id = "+ id));
+
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id= "+id));
+
+        return new PostsResponseDto(entity);
     }
 }
